3758c5c6040f8fdf39e8c53692a35caffade9df8
xingchuntao <xingchuntao@renrenche.com>
2018-10-13 21:10:02 +0800

    1

--- code/action.py
+++ code/action.py
@@ -30,7 +30,9 @@ def action(access_token,content):
 def doschedule():
     try:
         log.logPID()
-        access_token,content=info.getLocalConfig()
+        local_info=info.getLocalConfig()
+        access_token=local_info["access_token"]
+        content=local_info["content"]
         #schedule.every().day.at("18:00").do(action,access_token,content)
         #schedule.every().day.at("18:30").do(action,access_token,content)
         schedule.every(5).seconds.do(action,access_token,content)
--- code/info.py
+++ code/info.py
@@ -8,7 +8,7 @@ def getLocalConfig():
     try:
         with open('local.json','r') as f:
             info = json.load(f)
-            return info['access_token'],info['content']
+            return info
     except Exception as e:
         log.writelog()
    
